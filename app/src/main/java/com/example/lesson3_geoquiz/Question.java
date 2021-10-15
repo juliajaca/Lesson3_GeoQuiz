dfb989ddf42171a480213d4860c161bf4d2e98a6
jjaca <jjaca@cifpfbmoll.eu>
2021-10-15 21:04:05 +0200

    Creada y completada clase Question

---
+++ app/src/main/java/com/example/lesson3_geoquiz/Question.java
@@ -0,0 +1,28 @@
+package com.example.lesson3_geoquiz;
+
+public class Question {
+    //el texto de la pregunta mediante el ID y si es V o F
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue){
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
